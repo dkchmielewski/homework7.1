e5962be401a7a73174a83085bada4d0b6981375b
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-28 14:27:37 -0400

    initialized and declared int i in foor loop

--- src/Test.java
+++ src/Test.java
@@ -4,13 +4,12 @@ public class Test {
     public static void main(String[] args) {
         String[] names = new String[5];
         Scanner scanner = new Scanner(System.in);
-        int i;
-        for (i = 0; i < names.length; i++) {
+        for (int i = 0; i < names.length; i++) {
             System.out.println("Podaj imie");
             names[i] = scanner.nextLine();
         }
 
-        for (i = names.length - 1; i > -1; i--) {
+        for (int i = names.length - 1; i > -1; i--) {
             System.out.println("Czesc: "  + names[i]);
         }
     }
