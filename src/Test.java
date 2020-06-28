31e0a659f9c2bcb71f7964772431ae3b34156b21
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-28 07:30:59 -0400

    Added for loop to get user names, initialized index i with names.length -1.

--- src/Test.java
+++ src/Test.java
@@ -4,18 +4,13 @@ public class Test {
     public static void main(String[] args) {
         String[] names = new String[5];
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj imie");
-        names[0] = scanner.nextLine();
-        System.out.println("Podaj imie");
-        names[1] = scanner.nextLine();
-        System.out.println("Podaj imie");
-        names[2] = scanner.nextLine();
-        System.out.println("Podaj imie");
-        names[3] = scanner.nextLine();
-        System.out.println("Podaj imie");
-        names[4] = scanner.nextLine();
+        int i;
+        for (i = 0; i < names.length; i++) {
+            System.out.println("Podaj imie");
+            names[i] = scanner.nextLine();
+        }
 
-        for (int i = 4; i > -1; i--) {
+        for (i = names.length - 1; i > -1; i--) {
             System.out.println("Czesc: "  + names[i]);
         }
     }
